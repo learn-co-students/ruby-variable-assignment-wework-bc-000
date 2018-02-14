e6d16a8366c088dd85073d6617466048feb77fd8
Allie Goldberg <allie.goldberg@wework.com>
2018-02-14 22:10:29 +0000

    Done.

--- variable.rb
+++ variable.rb
@@ -1,6 +1,5 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-Hello = "Hello"
-puts "Hello World"
-phrase = "Hello World"
-puts phrase
+ greeting = "Hello World"
+ puts "Hello World"
+ puts greeting
